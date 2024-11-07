360b5e41045f9847b492ab2479b26ea4dedb320e
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-07 22:18:19 +0200

    add general turnouts

--- app/Http/Controllers/Api/V1/TurnoutController.php
+++ app/Http/Controllers/Api/V1/TurnoutController.php
@@ -4,13 +4,25 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Api\V1;
 
+use App\Enums\DataLevel;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\TurnoutResource;
 use App\Models\Election;
+use App\Models\Turnout;
+use Illuminate\Http\JsonResponse;
 
 class TurnoutController extends Controller
 {
-    public function turnout(Election $election)
+    public function general(Election $election): JsonResponse
     {
-        dd($election);
+        $result = Turnout::query()
+            ->whereBelongsTo($election)
+            ->forLevel(
+                level: DataLevel::TOTAL,
+            )
+            ->toBase()
+            ->first();
+
+        return response()->json(TurnoutResource::make($result));
     }
 }
