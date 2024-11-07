03f1c0d08876d696beed823f697c0e8ffbd3e261
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-07 22:26:23 +0200

    wip

--- resources/views/vendor/scramble/docs.blade.php
+++ resources/views/vendor/scramble/docs.blade.php
@@ -43,6 +43,11 @@
             return originalFetch(url, options);
         };
     </script>
+        <style>
+          h4{
+                display: none;
+            }
+        </style>
     @endprepend
 <elements-api
     id="docs"
