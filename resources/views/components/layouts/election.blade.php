36821b18fdaafb56a9a69d4f80ab9da99e407bf8
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-21 18:06:42 +0200

    add news in front

--- resources/views/components/layouts/election.blade.php
+++ resources/views/components/layouts/election.blade.php
@@ -1,11 +1,10 @@
 <x-layouts.base timeline>
     <x-timeline class="overflow-y-auto" />
-
     <div class="flex flex-col flex-1 overflow-y-auto" wire:scroll>
         <main id="content" class="flex-1 px-4 py-10 sm:px-6 lg:px-8">
             {{ $slot }}
         </main>
-
+        <livewire:news-feed />
         <x-site.footer />
     </div>
 </x-layouts.base>
---
+++ resources/views/livewire/news-feed.blade.php
@@ -0,0 +1,41 @@
+<section id="newsfeed" class="relative py-32 bg-primary-50 md:py-48">
+    <div class="container max-w-6xl">
+        <div
+            class="prose md:prose-lg lg:prose-xl max-w-none prose-headings:font-semibold prose-a:text-primary-800 prose-a:font-medium hover:prose-a:no-underline">
+            <h2 class="flex items-center gap-3 sm:gap-6">
+                <span>{{ __('app.newsfeed.title') }}</span>
+                <x-ri-rfid-line class="w-8 md:w-12" />
+            </h2>
+            <div class="mt-6 text-gray-500">
+                {!! Str::markdown(__('app.newsfeed.description')) !!}
+            </div>
+        </div>
+
+        <form>
+            {{ $this->form }}
+        </form>
+
+        <div x-on:refresh-feed="$wire.reload()" class="relative grid gap-4 mt-10 sm:gap-8">
+
+
+            {{ $this->posts->links(data: ['scrollTo' => false]) }}
+
+            @forelse ($this->posts as $post)
+                <x-news-feed-item :post="$post" />
+            @empty
+                <p>{{ __('app.newsfeed.empty') }}</p>
+            @endforelse
+
+            {{ $this->posts->links(data: ['scrollTo' => '#newsfeed']) }}
+        </div>
+    </div>
+
+    @script
+        <script>
+            $wire.on('$refresh', () => {
+                document.querySelector('#newsfeed').scrollIntoView()
+            });
+        </script>
+    @endscript
+
+</section>
