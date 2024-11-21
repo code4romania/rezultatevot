1b98691fcadf8bd9012663f9cfb8bd716edf6f48
Andrei Ioniță <hi@andrei.io>
2024-11-21 16:52:01 +0200

    proxy

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies('*');
+
         // Fix for `Route [login] not defined` exception
         // @see https://github.com/filamentphp/filament/discussions/5226#discussioncomment-10555366
         $middleware->redirectGuestsTo(fn () => Filament::getCurrentPanel()?->getLoginUrl() ?? route('front.index'));
