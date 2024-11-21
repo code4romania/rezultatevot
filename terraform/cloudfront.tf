e0f8e79b0801666947bec6bc8fba04c6bdefce1a
Andrei Ioniță <hi@andrei.io>
2024-11-21 16:33:53 +0200

    www redirect

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -31,6 +31,11 @@ resource "aws_cloudfront_distribution" "main" {
     origin_request_policy_id = aws_cloudfront_origin_request_policy.default.id
     viewer_protocol_policy   = "redirect-to-https"
     compress                 = true
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.www_redirect.arn
+    }
   }
 
   default_cache_behavior {
@@ -42,6 +47,11 @@ resource "aws_cloudfront_distribution" "main" {
 
     cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" #Managed-CachingDisabled
     origin_request_policy_id = aws_cloudfront_origin_request_policy.admin.id
+
+    function_association {
+      event_type   = "viewer-request"
+      function_arn = aws_cloudfront_function.www_redirect.arn
+    }
   }
 
   restrictions {
@@ -123,4 +133,12 @@ resource "aws_cloudfront_origin_request_policy" "admin" {
   query_strings_config {
     query_string_behavior = "all"
   }
+}
+
+resource "aws_cloudfront_function" "www_redirect" {
+  name    = "${local.namespace}-www-redirect"
+  runtime = "cloudfront-js-1.0"
+  comment = "Redirects ${var.domain_name} to www.${var.domain_name}"
+  publish = true
+  code    = file("${path.module}/functions/www-redirect.js")
 }
