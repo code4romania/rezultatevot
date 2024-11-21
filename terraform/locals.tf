fcfe867b366fc437bc6f8b01a917f6ef7c54d5ff
Andrei Ioniță <hi@andrei.io>
2024-11-21 15:55:28 +0200

    infra: bump version

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,7 +2,7 @@ locals {
   namespace = "rezultatevot-v2-${var.env}"
   image = {
     repo = "code4romaina/rezultatevot",
-    tag  = "0.1.0"
+    tag  = "0.1.1"
   }
   #   availability_zone = data.aws_availability_zones.current.names[0]
 
