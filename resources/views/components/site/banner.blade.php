dbacf6201a67fd395fa038be25ecf54f9554c340
Andrei Ioniță <hi@andrei.io>
2024-11-22 15:36:20 +0200

    update logo

--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -14,7 +14,7 @@
             target="_blank"
             rel="noopener"
             class="flex items-center w-full max-w-2xl gap-3 text-sm hover:text-blue-600 focus:text-blue-600 focus:outline-0 hover:underline focus:underline">
-            <x-icon-commitglobal class="h-6 sm:h-8 shrink-0" />
+            <x-icon-code4commit class="h-6 sm:h-8 shrink-0" />
 
             <span>{{ __('app.banner') }}</span>
         </a>
