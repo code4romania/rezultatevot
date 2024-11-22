be58d5bb688a2928577b184fcbe20b8a70c85d7c
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-22 15:01:27 +0200

    fix embed

--- app/Livewire/EmbedButton.php
+++ app/Livewire/EmbedButton.php
@@ -4,14 +4,24 @@ declare(strict_types=1);
 
 namespace App\Livewire;
 
+use Livewire\Attributes\Url;
 use Livewire\Component;
 
 class EmbedButton extends Component
 {
     public string $url;
 
+    public array $queryParams=[];
+
+
+    public function mount(): void
+    {
+        $this->queryParams = request()->query();
+    }
+
     public function render()
     {
+
         return view('livewire.embed-button');
     }
 }
--- resources/js/embed.js
+++ resources/js/embed.js
@@ -4,11 +4,20 @@ export default () => ({
 
     init() {
         this.url = this.$wire.url;
+
+
     },
 
     copy() {
+
+        let searchParams = new URLSearchParams();
+        for (const key in this.$wire.queryParams) {
+            searchParams.append(key, this.$wire.queryParams[key]);
+        }
+        let url = new URL(this.url);
+        url.search = searchParams.toString();
         const code =
-            `<iframe id="${this.id}" src="${this.url}" style="width:100%;height:100vh"></iframe>` +
+            `<iframe id="${this.id}" src="${url.toString()}" style="width:100%;height:100vh"></iframe>` +
             `<script src="https://cdn.jsdelivr.net/npm/@iframe-resizer/parent@5.3.2"></script>` +
             `<script>iframeResize({license:'GPLv3'},'#${this.id}');</script>`;
 
