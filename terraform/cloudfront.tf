9891595a4dfcde4a55c2377ab03986a8d2900782
Andrei Ioniță <hi@andrei.io>
2024-11-24 01:11:07 +0200

    infra: remove api cloudfront cache

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -34,7 +34,7 @@ resource "aws_cloudfront_distribution" "main" {
     allowed_methods          = ["GET", "HEAD", "OPTIONS"]
     cached_methods           = ["GET", "HEAD", "OPTIONS"]
     target_origin_id         = aws_lb.main.dns_name
-    cache_policy_id          = aws_cloudfront_cache_policy.default.id
+    cache_policy_id          = "4135ea2d-6df8-44a3-9df3-4b5a84be39ad" #Managed-CachingDisabled
     origin_request_policy_id = aws_cloudfront_origin_request_policy.default.id
     viewer_protocol_policy   = "redirect-to-https"
     compress                 = true
