1351e7d48aeb4a615fd3d097ab65fbdafb36574f
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-23 13:28:41 +0200

    remove down

--- database/migrations/2024_11_23_112207_add_description_to_users.php
+++ database/migrations/2024_11_23_112207_add_description_to_users.php
@@ -15,14 +15,4 @@ return new class extends Migration
             $table->text('description')->nullable();
         });
     }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('description');
-        });
-    }
 };
