fbcee5a91118acd50b2e2bb4def53a8ab458bf01
Andrei Ioniță <hi@andrei.io>
2024-11-24 14:32:40 +0200

    fix: banner social test (#87)

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,8 @@ import Clipboard from '@ryangjchandler/alpine-clipboard';
 import embed from './embed.js';
 import map from './map.js';
 
+import.meta.glob(['../images/**']);
+
 Alpine.plugin(Clipboard);
 Alpine.data('embed', embed);
 Alpine.data('map', map);
