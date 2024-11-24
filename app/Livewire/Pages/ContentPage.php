ebce80bf2c494f60aa9b13d82ca6bb63ec9db559
Andrei Ioniță <hi@andrei.io>
2024-11-24 14:42:28 +0200

    wip

--- app/Livewire/Pages/ContentPage.php
+++ app/Livewire/Pages/ContentPage.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace App\Livewire\Pages;
 
 use App\Models\Page;
+use Illuminate\View\View;
 use Livewire\Component;
 
 class ContentPage extends Component
 {
     public Page $page;
 
-    public function render()
+    public function render(): View
     {
         seo()
             ->title($this->page->title);
