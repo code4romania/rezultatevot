990536c85616a56b0384acf5a5e2717b29e70eea
Andrei Ioniță <hi@andrei.io>
2024-11-26 18:02:38 +0200

    fix: footer (#104)

--- resources/views/components/navigation-item.blade.php
+++ resources/views/components/navigation-item.blade.php
@@ -1,14 +1,17 @@
-@props(['mobile' => false])
+@props([
+    'mobile' => false,
+    'primary' => false,
+])
 
 <a
     {{ $attributes->merge([
             'href' => $item->url,
             'wire:navigate' => $item->target === '_self',
         ])->class([
-            'font-medium leading-tight',
-            'text-primary-900 hover:bg-primary-50',
+            ' leading-tight',
+            $primary ? 'font-medium text-primary-900 hover:bg-primary-50' : 'text-gray-600 hover:text-gray-900',
             // $isCurrent() ? 'bg-primary-50' : '',
-            $mobile ? 'flex px-2 py-3' : 'px-3 py-2 rounded',
+            $primary ? ($mobile ? 'flex px-2 py-3' : 'px-3 py-2 rounded') : '',
         ]) }}>
     {{ $item->title }}
 </a>
--- resources/views/components/site/banner.blade.php
+++ resources/views/components/site/banner.blade.php
@@ -10,11 +10,11 @@
         @endif
 
         <a
-            href="https://www.commitglobal.org"
+            href="https://www.code4.ro"
             target="_blank"
             rel="noopener"
             class="flex items-center w-full max-w-2xl gap-3 text-sm hover:text-blue-600 focus:text-blue-600 focus:outline-0 hover:underline focus:underline">
-            <x-icon-code4commit class="h-6 sm:h-8 shrink-0" />
+            <x-icon-code4romania class="h-6 sm:h-8 shrink-0" />
 
             <span>{{ __('app.banner') }}</span>
         </a>
