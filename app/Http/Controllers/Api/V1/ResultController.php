2c14f480f2596da269c00b613a3d6a4cebd81d01
Lupu Gheorghe <gheorghelupu17@gmail.com>
2024-11-28 16:16:18 +0200

    zwip

--- app/Http/Controllers/Api/V1/ResultController.php
+++ app/Http/Controllers/Api/V1/ResultController.php
@@ -7,21 +7,35 @@ namespace App\Http\Controllers\Api\V1;
 use App\Enums\DataLevel;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\Result\ResultResource;
-use App\Models\Record;
+use App\Models\Candidate;
+use App\Models\Election;
+use App\Models\Party;
+use App\Models\Vote;
+use App\Repositories\VotesRepository;
 use Illuminate\Http\Resources\Json\JsonResource;
+use stdClass;
 
 class ResultController extends Controller
 {
     /**
      * @operationId ResultTotal
      */
-    public function total(): JsonResource
+    public function total(Election $election): JsonResource
     {
-        $result= Record::forLevel(
+        $result = VotesRepository::getMapDataForLevel(
+            election: $election,
             level: DataLevel::TOTAL,
-            aggregate: true,
-        )->get();
-        dd($result);
+        )->mapWithKeys(function (stdClass $vote) {
+            return [
+                $vote->place => [
+                    'value' => percent($vote->votes, $vote->total_votes, formatted: true),
+                    'type' => $vote->votable_type,
+                    'id' => $vote->votable_id,
+                ],
+            ];
+        });
+
+
 
         return ResultResource::make();
     }
