f70647f923f939abb23465b315391f54fd281bb3
Andrei Ioniță <hi@andrei.io>
2024-11-30 13:43:07 +0200

    fix: default import disk to filesystem disk

--- config/import.php
+++ config/import.php
@@ -6,7 +6,7 @@ return [
 
     'awk_path' => env('AWK_PATH', '/usr/bin/awk'),
 
-    'disk' => env('IMPORT_DISK', 'local'),
+    'disk' => env('IMPORT_DISK', env('FILESYSTEM_DISK', 'local')),
 
     'independent_candidate_prefix' => env('IMPORT_INDEPENDENT_CANDIDATE_PREFIX', 'CANDIDAT INDEPENDENT - '),
     'candidate_votes_suffix' => env('IMPORT_CANDIDATE_VOTES_SUFFIX', '-voturi'),
