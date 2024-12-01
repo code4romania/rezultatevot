2fdc829310e5072c4d16f577e0737eb46135a416
Andrei Ioniță <hi@andrei.io>
2024-12-01 14:20:21 +0200

    fix: cards (#123)

--- resources/views/components/stats/records.blade.php
+++ resources/views/components/stats/records.blade.php
@@ -1,11 +1,11 @@
 <section>
     <h1 class="mb-6 text-3xl font-bold">Procesul electoral</h1>
 
-    <div class="grid grid-cols-2 gap-4 lg:grid-cols-4">
+    <div class="grid gap-4 sm:grid-cols-2">
 
         @foreach ($stats as $key => $value)
             <x-stats.card>
-                <dt class="text-sm font-medium leading-tight text-gray-700 truncate">
+                <dt class="text-sm font-medium leading-tight text-gray-700">
                     {{ $key }}
                 </dt>
                 <dd class="text-2xl font-semibold leading-tight text-gray-900">
@@ -14,5 +14,4 @@
             </x-stats.card>
         @endforeach
     </div>
-
 </section>
