a586d4c4e286a98b2931ab2eabf43ad70a26db96
Andrei Ioniță <hi@andrei.io>
2024-12-02 18:46:28 +0200

    infra: version bump (#148)
    * infra: version bump
    
    * infra: scale down services
    
    * infra: scale down ecs

--- terraform/service_app.tf
+++ terraform/service_app.tf
@@ -7,7 +7,7 @@ module "ecs_app" {
 
   name         = "${local.namespace}-app"
   cluster_name = module.ecs_cluster.cluster_name
-  min_capacity = 9
+  min_capacity = 2
   max_capacity = 18
 
   deployment_minimum_healthy_percent = 50
@@ -25,8 +25,8 @@ module "ecs_app" {
   lb_health_check_enabled = true
   lb_path                 = "/up"
 
-  container_memory_soft_limit = 1536
-  container_memory_hard_limit = 2048
+  container_memory_soft_limit = 768
+  container_memory_hard_limit = 1536
 
   log_group_name                 = module.ecs_cluster.log_group_name
   service_discovery_namespace_id = module.ecs_cluster.service_discovery_namespace_id
