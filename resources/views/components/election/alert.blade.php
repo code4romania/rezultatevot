fa919c489dcaa5e1e317cf3d626d0620ede58e09
Andrei Ioniță <hi@andrei.io>
2024-12-06 19:39:27 +0200

    fix: banner

--- resources/views/components/election/alert.blade.php
+++ resources/views/components/election/alert.blade.php
@@ -1,7 +1,5 @@
 <x-alert {{ $attributes->class('bg-red-100') }}>
-    <div class="flex items-center gap-2">
-        <x-heroicon-s-exclamation-triangle class="w-5 h-5 text-red-600" />
-
+    <div class="prose max-w-none">
         {{ $alert }}
     </div>
 </x-alert>
